35f14a20c9a72d047ed79907962dfcd05a4ac1ee
JenniferXuXia <jennifer.xu.xia@gmail.com>
2022-11-08 22:48:39 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import tweepy
+import config
+
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
